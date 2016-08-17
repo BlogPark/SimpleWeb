097acae736ac8c35266338aa373c3b88bed39b1b
AnEmptyCup <chenghang1937@hotmail.com>
2016-08-17 21:31:07 +0800

    提交代码

---
+++ SimpleWeb/Areas/NewTemplateArea/Controllers/RegisterController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleWeb.Areas.NewTemplateArea.Models;
+using SimpleWeb.Common;
+using SimpleWeb.Controllers;
+using SimpleWeb.DataBLL;
+using SimpleWeb.DataModels;
+
+namespace SimpleWeb.Areas.NewTemplateArea.Controllers
+{
+    public class RegisterController : Controller
+    {
+        //注册页面
+        // GET: /NewTemplateArea/Register/
+
+        private MemberInfoBLL bll = new MemberInfoBLL();
+        private WebSettingsBLL webbll = new WebSettingsBLL();
+        private WebSettingsModel web;
+        public RegisterController()
+        {
+            web = webbll.GetWebSiteModel();
+        }
+        public ActionResult Index(string msd)
+        {
+            NewRegisterViewModel model = new NewRegisterViewModel();
+            model.member = new MemberInfoModel();
+            model.member.MemberPhone = msd;
+            model.regintable = bll.GetReginTableListModel(1);
+            ViewBag.PageTitle = web.WebName;
+            ViewData["Error"] = "";
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Index(MemberInfoModel member)
+        {
+            if (member != null)
+            {
+                member.LogPwd = DESEncrypt.Encrypt(member.LogPwd, AppContent.SecrectStr);
+                string row = bll.AddMemberInfo(member);
+                if (row == "1")
+                {
+                    return RedirectToAction("Index", "Login", new { area = "WebFrontArea" });
+                }
+                else
+                {
+                    ViewData["Error"] = row.Substring(1);
+                }
+            }
+            NewRegisterViewModel model = new NewRegisterViewModel();
+            model.member = member;
+            model.member.MemberPhone = member.MemberPhone;
+            model.regintable = bll.GetReginTableListModel(1);
+            ViewBag.PageTitle = web.WebName;
+            return View(model);
+        }
+
+    }
+}
---
+++ SimpleWeb/Areas/NewTemplateArea/Models/NewRegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using SimpleWeb.DataModels;
+
+namespace SimpleWeb.Areas.NewTemplateArea.Models
+{
+    [Serializable]
+    [DataContract]
+    public class NewRegisterViewModel
+    {
+        /// <summary>
+        /// 会员
+        /// </summary>
+        [DataMember]
+        public MemberInfoModel member { get; set; }
+        [DataMember]
+        public List<ReginTableModel> regintable { get; set; }
+    }
+}
